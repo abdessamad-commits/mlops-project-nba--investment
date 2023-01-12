4b5ca96083b9645f3febd744279fbbf7e9fefe8e
abdessamad baahmed <abdessamadbaahmed@MacBook-Air-de-abdessamad.local>
2023-01-12 14:27:26 +0100

    added new functions to test the models stored on the registry

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -45,7 +45,7 @@ def preprocessing_data(df):
     df["3P%"].fillna(df["3P%"].mean(), inplace=True)
     return df
 
-def test_model(run_id, X_test, y_test):
+def test_model_by_run_id(run_id, X_test, y_test):
     """
     this function tests the model on the test data and prints the evaluation metrics in order to go to production
 
@@ -54,7 +54,7 @@ def test_model(run_id, X_test, y_test):
     :param X_test: test features
     :param y_test: test target
 
-    :return: None
+    :return: dictionary with the evaluation metrics
     """
     with mlflow.start_run():
         
@@ -76,3 +76,60 @@ def test_model(run_id, X_test, y_test):
             "recall": recall_score(y_test, y_test_pred),
             "f1": f1_score(y_test, y_test_pred),
         }
+        
+def test_model_by_regsitry_version(model_name, model_version, X_test, y_test):
+    """
+    this function tests the model on the test data and prints the evaluation metrics in order to go to production
+    
+    :param model_name: name of the model
+    :param model_version: version of the model
+    :param X_test: test features
+    :param y_test: test target
+    
+    :return: dictionary with the evaluation metrics
+    """
+    with mlflow.start_run():
+        
+        mlflow.set_tag("holdout_set", "testing set")
+        
+        loaded_model = mlflow.pyfunc.load_model(
+            model_uri=f"models:/{model_name}/{model_version}")
+        y_test_pred = loaded_model.predict(X_test).round()
+        
+        mlflow.log_metric("accuracy", accuracy_score(y_test, y_test_pred))
+        mlflow.log_metric("precision", precision_score(y_test, y_test_pred))
+        mlflow.log_metric("recall", recall_score(y_test, y_test_pred))
+        mlflow.log_metric("f1_score", f1_score(y_test, y_test_pred))
+        mlflow.set_tag("model_name", model_name)
+        mlflow.set_tag("model_version", model_version)
+        
+        return {
+            "model_name": model_name,
+            "model_version": model_version,
+            "accuracy": accuracy_score(y_test, y_test_pred),
+            "precision": precision_score(y_test, y_test_pred),
+            "recall": recall_score(y_test, y_test_pred),
+            "f1": f1_score(y_test, y_test_pred),
+        }
+        
+def test_model_by_regsitry_all_versions(model_name, X_test, y_test, sort_by="f1"):
+    """
+    the function tests all the versions of the model and returns a DataFrame with the evaluation metrics
+    
+    :param model_name: name of the model
+    :param X_test: test features
+    :param y_test: test target
+    
+    :return: DataFrame with the evaluation metrics
+    """
+    i = 0
+    res = []
+    while True:
+        try:
+            res.append(test_model_by_regsitry_version(model_name, i+1, X_test, y_test))
+        except Exception:
+            break
+        i+=1
+    return pd.DataFrame(res).sort_values(by=sort_by, ascending=False)
+        
+        
