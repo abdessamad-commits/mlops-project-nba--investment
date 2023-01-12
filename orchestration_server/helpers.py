647662c1b85ccd8db4fdcb4a19d1e2727d1d2da1
abdessamad baahmed <abdessamadbaahmed@MacBook-Air-de-abdessamad.local>
2023-01-12 16:53:34 +0100

    added an orchestration service

---
+++ orchestration_server/helpers.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def convert_numerical_columns_to_float(df):
+    """
+    this function convert all numerical columns of a DataFrame to float
+
+    :param df: DataFrame
+
+    ;return: DataFrame
+    """
+    numerical_cols = df.select_dtypes(include=["int", "float"]).columns
+    df[numerical_cols] = df[numerical_cols].astype(float)
+    return df
